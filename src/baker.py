a87ddb0a2443e32261a751bf4d274b8824175ec9
Cindy <weng0@hm.edu>
2025-09-29 01:15:54 +0200

    in class Baker add new function setNewDough and partly implement mix_ingredients with match case and inputs in a while-loop

--- src/baker.py
+++ src/baker.py
@@ -1,19 +1,38 @@
 '''
 Python Klasse für den Bäcker
 '''
+from dough import Dough
 
 class Baker:
     def __init__(self, first_name, surname, salary):
         self.first_name = first_name
         self.surname = surname
         self.salary = salary
+        self.dough_0 = None
 
-    def mix_ingredients(self, something):
+    def setNewDough(self):
+        self.dough_0 = Dough()
+
+    def mix_ingredients(self):
         ''' Alle Zutaten für das Brot werden in einer Masse vermischt.
         Eingabeparametern sind die Zutaten
         Rückgabewert ist der Teig
+        Die Funktion fordert solange nach einer Eingabe, bis man alle fehlende Zutaten eingegeben hat
         '''
-        pass
+        self.setNewDough()
+        while self.dough_0.isFinished() == False:
+            print('Was wollen Sie hinzugeben?\n', '1.Wasser\n', '2.Mehl\n')
+            choice = input()
+            match choice:
+                case '1':
+                    print('Bitte die korrekte Menge an Wasser eingießen.'+'\n')
+                    inp = input()
+                    self.dough_0.water = inp
+                case '2':
+                    print('Bitte die korrekte Menge an Mehl eingeben.'+'\n')
+                    inp = input()
+                    self.dough_0.flour = inp
+        return self.dough_0
 
     def knead_dough(self, something):
         '''Durch das Methode Kneten wird der Teig, der als Eingabeparameter übergeben wird, bearbeitet.'''
@@ -22,3 +41,6 @@ class Baker:
     def form_dough(self, something):
         '''Durch dieser Methode wird der Teig in kleineren Teile zerteilt und geformt. Der Teig wird als Parameter übergeben.'''
         pass
+
+baker = Baker('Max','Mustermann', 3800)
+baker.mix_ingredients()
