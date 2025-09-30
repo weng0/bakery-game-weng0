850f5f1d7e73228a3feb30947a81a65acc02e888
Cindy <weng0@hm.edu>
2025-09-30 16:20:01 +0200

    commit class Game and def play()

--- src/game.py
+++ src/game.py
@@ -1,3 +1,25 @@
 '''
 Hier wird die Spielmechanik implementiert
 '''
+from baker import Baker
+from oven import Oven
+from bread import Bread
+
+class Game:
+    def __init__(self):
+        pass
+
+    def play(self):
+        baker = Baker('Max','Mustermann', 3800)
+        baker.setNewDough()
+        baker.mix_ingredients()
+        baker.knead_dough()
+        baker.devide_and_form()
+        print(baker.dough_list)
+        print(len(baker.dough_list))
+        print(baker.dough_list[10].ingredients)
+
+        baker.form_bread() # 
+
+game = Game()
+game.play()
