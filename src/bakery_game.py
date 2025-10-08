b57241a09cd20c72afe5c9bab3b780d9affede7b
Cindy <weng0@hm.edu>
2025-10-08 20:14:46 +0200

    added class Customer and defined member variable

--- src/bakery_game.py
+++ src/bakery_game.py
@@ -3,5 +3,3 @@ Hiermit wird ein neues Bäckerei Simulationsspiel gestartet
 '''
 # from game import Game
 
-# bakery_game = Game()
-# bakery_game.start()
---
+++ src/customer.py
@@ -0,0 +1,14 @@
+'''
+Kunden
+'''
+
+class Customer:
+    def __int__(self, name, email, tel, strasse, plz, ort):
+        self.company_name = name
+        self.email = email
+        self.tel = tel
+        self.strasse = strasse
+        self.plz = plz
+        self.ort = ort
+
+    
