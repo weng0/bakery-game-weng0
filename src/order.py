a5c433d4467c9e22e01c0df3e5c9785b8579afb4
Cindy <weng0@hm.edu>
2025-10-10 19:43:20 +0200

    added function zip_data()

--- src/order.py
+++ src/order.py
@@ -1,7 +1,6 @@
 '''Bestellung
 '''
-brotsorte = {'Breze':1.20, 'Croissant':2.45}
-menge = [10, 20]
+
 class Order:
     __idCounter : int = 123
     def __init__(self): # take out
@@ -11,7 +10,19 @@ class Order:
         self.product_data = None # setProduct()
         self.order_ID = Order.__idCounter
     
-
+    def zip_data(self,bread_type,quantity, pos_price):# Brotsorte ist ein Dict
+        bread_lst = []
+        einzel_preis = []
+        if len(bread_type) == len(quantity):
+            for index in range(len(quantity)):
+                price = bread_type[index][1] * quantity[index]
+                bread_lst.append(bread_type[index][0])
+                einzel_preis.append(bread_type[index][1])
+                pos_price.append(price)
+        data_kombi_2 = zip(bread_lst,einzel_preis,quantity, pos_price)
+        self.product_data = data_kombi_2
+        return data_kombi_2
 
     # def setProduct(self):
     #     self.product_data.update({})
+
